bc496870184637e3779856af349d06d7c9c998b8
eldritchreality <eldritchreality@gmail.com>
2015-01-31 19:19:15 +0000

    added basic unit tests for the pokemon and personality files

---
+++ test/test_personalities_json.js
@@ -0,0 +1,34 @@
+var should = require("chai").should(),
+    fs = require("fs");
+
+describe("The pokemon data file", function(){
+    var filepath = "./data/pokemon.json"
+    
+    it('should exist',function(done){
+       var dataFileExists = fs.existsSync(filepath);
+       
+       dataFileExists.should.be.true;
+       done();
+   });
+    
+   it('should load', function(done){
+       fs.readFile(filepath,done);
+       
+   });
+    
+   it('should parse as valid JSON', function(done){
+       fs.readFile(filepath,function(err,data){
+           var parsedDate = null;
+           if (err) throw new Error(err);
+           parsedData = JSON.parse(data.toString());
+           parsedData.should.satisfy(function(mightBeArray){return typeof mightBeArray.length == "number" && mightBeArray.length > 0});
+           done();
+       })
+       
+       
+   })
+    
+    
+    
+});
+
